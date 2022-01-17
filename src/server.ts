01cd241ccf6715914c8c8c3d6c3d0be545d34000
Silas Sena <silasadssn@gmail.com>
2022-01-16 23:54:01 -0300

    testando nova estrutura de deployment para azure

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,13 @@ import express from 'express';
 const app = express();
 
 app.get('/api/', (_req, res) => {
-  res.status(200).send('Hello Word! How are you my friend?')
+  res.status(200).json('Hello Word!')
+})
+
+app.post('/api/v1/', async (_req, res) => {
+  res.status(200).json({
+    message: 'Success!',
+  })
 })
 
 const port = process.env.PORT || 80;
